ff1b509863a8e116e020f500269c7e6fa501a5d6
Saad-Babar <saadmubeen2004@gmail.com>
2023-09-22 17:11:08 +0500

    frst

---
+++ index.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    // nav: true,
+    loop: true,
+    margin: 20,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive : {
+        0 : {
+            items: 1,
+        },
+        768 : {
+            items: 3,
+        },
+    }
+});
+
+$('.owl-carousel1').owlCarousel({
+    // nav: true,
+    loop: true,
+    margin: 10,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    // autoplayHoverPause: true,
+});
+
+

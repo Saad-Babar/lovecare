6c25fbbde7dca641bcae0ef00fc2d28fc805d445
Saad-Babar <saadmubeen2004@gmail.com>
2023-09-22 19:52:50 +0500

    alladd

---
+++ index.js
@@ -0,0 +1,35 @@
+$('.owl-crl1').owlCarousel({
+    // nav: true,
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive : {
+        0 : {
+            items: 1,
+        },
+        768 : {
+            items: 3,
+        },
+    }
+});
+
+$('.olu-craosal').owlCarousel({
+    // nav: true,
+    loop: true,
+    margin: 25,
+    autoplay: true,
+    center: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    slideBy:1,
+    responsive : {
+        0 : {
+            items: 1,
+        },
+        768 : {
+            items: 3,
+        },
+    }
+});
